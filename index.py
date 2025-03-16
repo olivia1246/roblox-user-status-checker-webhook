a82c6b964a2995e9326f7a477a6cbf98bdd67fa7
olivia1246 <135425765+olivia1246@users.noreply.github.com>
2025-03-16 16:55:20 +0200

    Update index.py

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import json
 import os
 from datetime import datetime
 
-username = "Sophia3Soso0"
+username = "SeasonableJourney"
 roblosecurity_cookie = os.getenv('ROBLOSECURITY_COOKIE')
 discord_webhook_url = os.getenv('DISCORD_WEBHOOK_URL')
 
